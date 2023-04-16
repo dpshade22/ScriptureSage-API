23750c51d911ba4dd9070505669188ea60e056fe
Dylan Shade <63427984+dpshade22@users.noreply.github.com>
2023-04-16 03:36:33 -0400

    added functions for refactoring

--- routes.jl
+++ routes.jl
@@ -1,6 +1,37 @@
 using CSV, DataFrames, HTTP, URIs, JSON, Distances, OpenAI, LinearAlgebra, DotEnv
 DotEnv.config()
 
+api_key = ENV["OPENAI_API_KEY"]
+
+function find_similarities(api_key, num_search_terms, query_params, embeddingsDF)
+    search_term = query_params["query"]
+    search_term_vector = Vector{Float64}(create_embeddings(api_key, search_term, engine="text-embedding-ada-002").response.data[1].embedding)
+
+    if num_search_terms == 2
+        search_term_2 = query_params["query_2"]
+        # Get the embeddings for the second search term
+        search_term_vector_2 = JSON.parse(create_embeddings(api_key, search_term_2, engine="text-embedding-ada-002"))
+        # Calculate the combined embeddings and similarities
+        embeddingsDF.similarities = cosine_similarity.(eachrow(embeddingsDF.embedding), combined_vector)
+    else
+        embeddingsDF.similarities = [cosine_similarity(x[1], search_term_vector) for x in eachrow(embeddingsDF.embedding)]
+    end
+
+    # Sort and get the top 5 results
+    return sort(embeddingsDF, :similarities, rev=true)[1:5, Not([:embedding, :Column1])]
+end
+
+function load_embeddings(embeddingByChapterCSV, embeddingByVerseNTCSV)
+    embeddingsByChapter = CSV.read(embeddingByChapterCSV, DataFrame)
+    embeddingsByChapter.embedding = [Vector{Float64}(JSON.parse(x)) for x in embeddingsByChapter.embedding]
+    embeddingsByChapter.location = [embeddingsByChapter.Book[i] * " " * string(embeddingsByChapter.Chapter[i]) for i in 1:length(embeddingsByChapter.Book)]
+    embeddingsByChapter.verse = [embeddingsByChapter.Verses[i] for i in 1:length(embeddingsByChapter.Book)]
+
+    embeddingsByVerseNT = CSV.read(embeddingByVerseNTCSV, DataFrame)
+    embeddingsByVerseNT.embedding = [Vector{Float64}(JSON.parse(x)) for x in embeddingsByVerseNT.embedding]
+    return embeddingsByChapter, embeddingsByVerseNT
+end
+
 function cosine_similarity(a, b)
     return dot(a, b) / (norm(a) * norm(b))
 end
@@ -12,14 +43,7 @@ function server(req)
     parsed_uri = URIs.URI(uri)
 
     # Set the OpenAI API key
-    api_key = ENV["OPENAI_API_KEY"]
-    embeddingsByChapter = CSV.read("embeddings/chapter/KJV_Bible_Embeddings_by_Chapter.csv", DataFrame)
-    embeddingsByChapter.embedding = [Vector{Float64}(JSON.parse(x)) for x in embeddingsByChapter.embedding]
-    embeddingsByChapter.location = [embeddingsByChapter.Book[i] * " " * string(embeddingsByChapter.Chapter[i]) for i in 1:length(embeddingsByChapter.Book)]
-    embeddingsByChapter.verse = [embeddingsByChapter.Verses[i] for i in 1:length(embeddingsByChapter.Book)]
-
-    embeddingsByVerseNT = CSV.read("embeddings/verse/nt/KJV_NT_Bible_Embeddings_by_Verse.csv", DataFrame)
-    embeddingsByVerseNT.embedding = [Vector{Float64}(JSON.parse(x)) for x in embeddingsByVerseNT.embedding]
+    embeddingsByChapter, embeddingsByVerseNT = load_embeddings("embeddings/chapter/KJV_Bible_Embeddings_by_Chapter.csv", "embeddings/verse/nt/KJV_NT_Bible_Embeddings_by_Verse.csv")
 
     # Handle the /hello endpoint
     if uri == "/hello"
@@ -29,39 +53,19 @@ function server(req)
 
     # Handle the /add endpoint
     if startswith(parsed_uri.path, "/search")
-        println(parsed_uri)
         query_params = URIs.queryparams(parsed_uri.query)
-        println(query_params)
 
         # Check if the required query parameters are present
         if haskey(query_params, "search_by") && haskey(query_params, "query")
             # Parse the query parameters as integers
-            search_terms = haskey(query_params, "query_2") ? "2" : "1"
+            search_terms = haskey(query_params, "query_2") ? 2 : 1
             embeddingsDF = query_params["search_by"] == "chapter" ? embeddingsByChapter : embeddingsByVerseNT
         else
             # Respond with an error message if the query parameters are missing
             return HTTP.Response(400, "Missing query parameters 'search_by' and 'query'")
         end
 
-        search_term = query_params["query"]
-
-        # Get the embeddings for the first search term
-        search_term_vector = Vector{Float64}(create_embeddings(api_key, search_term, engine="text-embedding-ada-002").response.data[1].embedding)
-
-        # Check if the second search term is provided
-        # If the second search term is provided
-        if search_terms == "2"
-            search_term_2 = query_params["query_2"]
-            # Get the embeddings for the second search term
-            search_term_vector_2 = JSON.parse(create_embeddings(api_key, search_term_2, engine="text-embedding-ada-002"))
-            # Calculate the combined embeddings and similarities
-            embeddingsDF.similarities = cosine_similarity.(eachrow(embeddingsDF.embedding), combined_vector)
-        else
-            embeddingsDF.similarities = [cosine_similarity(x[1], search_term_vector) for x in eachrow(embeddingsDF.embedding)]
-        end
-
-        # Sort and get the top 5 results
-        foundDF = sort(embeddingsDF, :similarities, rev=true)[1:5, Not([:embedding, :Column1])]
+        foundDF = find_similarities(api_key, search_terms, query_params, embeddingsDF)
 
         json_array = []
         for i in eachindex(foundDF[!, "location"])
